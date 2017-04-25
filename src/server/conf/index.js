92a7626eebdaaba8d80888ef775796894ccb8fda
Igor Vukelić <igvukelic@gmail.com>
2017-04-25 19:09:03 +0200

    heroku fix3

--- src/server/conf/index.js
+++ src/server/conf/index.js
@@ -1,8 +1,8 @@
 import defaultConf from '../../../conf/app.json'
-const isDev: boolean = process.env.NODE_ENV === 'development'
-const localConf: Object = isDev ? require('../../../conf/app.local.json') : {}
+const isDev = process.env.NODE_ENV === 'development'
+const localConf = isDev ? require('../../../conf/app.local.json') : {}
 
-const conf: Object = {
+const conf = {
   ...defaultConf,
   ...localConf
 }
